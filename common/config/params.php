311b93abc1905ea85359cf9dcdf16be2d43882d1
vadimlekhter <v_lehter@mail.ru>
2020-12-30 21:13:30 +0300

    New 'support email'

--- common/config/params.php
+++ common/config/params.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
+    'supportEmail' => 'vadimlekhter@yandex.ru',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
     'user.passwordResetTokenExpire' => 3600,
