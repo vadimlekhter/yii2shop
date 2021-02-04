887addff747ce847a752780179f2c7ccfcdc706f
vadimlekhter <v_lehter@mail.ru>
2021-02-04 12:25:01 +0300

    CORS for API Task Controller

--- frontend/modules/api/controllers/TaskController.php
+++ frontend/modules/api/controllers/TaskController.php
@@ -5,8 +5,30 @@ namespace frontend\modules\api\controllers;
 
 
 use common\models\Task;
+use yii\filters\Cors;
 
 class TaskController extends \yii\rest\ActiveController
 {
     public $modelClass = Task::class;
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        // remove authentication filter
+        $auth = $behaviors['authenticator'];
+        unset($behaviors['authenticator']);
+
+        // add CORS filter
+        $behaviors['corsFilter'] = [
+            'class' => Cors::class,
+        ];
+
+        // re-add authentication filter
+        $behaviors['authenticator'] = $auth;
+        // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
+        $behaviors['authenticator']['except'] = ['options'];
+
+        return $behaviors;
+    }
 }
